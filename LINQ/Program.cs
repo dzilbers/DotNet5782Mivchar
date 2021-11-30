606005d8c310e1b571d1f0697ac6819bb40a4e97
Dan Zilberstein <dzilbers@gmail.com>
2021-11-30 13:44:01 +0200

    LINQ & basic data binding

---
+++ LINQ/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace LINQ
+{
+    struct MyStruct
+    {
+        int boo;
+        public int Boo
+        {
+            get
+            {
+                Console.WriteLine("Getter: " + boo);
+                return boo;
+            }
+            set => boo = value;
+        }
+        public MyStruct(int value) => boo = value;
+        public override string ToString() => "Boo: " + boo;
+    }
+    class Program
+    {
+        static List<MyStruct?> list = new() { new(1), new(2), new(3), new(4) };
+        static void Main(string[] args)
+        {
+            // ================== basic LINQ and deferred execution ==================
+            //IEnumerable<MyStruct?> coll = list;
+
+            //Console.WriteLine("Start");
+            //Console.ReadKey();
+            //var obj1 = list.Find(o => o?.Boo > 0); // List<> and IEnumerable<> functions
+            //Console.WriteLine("List tracked");
+            //Console.ReadKey();
+            //var obj2 = coll.FirstOrDefault(o => o?.Boo == 2);  // IEnumerable<> functions
+            //Console.WriteLine("Collection tracked");
+            //Console.ReadKey();
+
+            //var result = //coll.Where(item => item?.Boo % 2 == 0).Select(item => "Boo" + item?.Boo);
+            //    from item in coll
+            //    where item?.Boo % 2 == 0
+            //    select "Boo" + item?.Boo;
+
+            //Console.WriteLine("Result Assigned");
+            //Console.ReadKey();
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine("foreach " + item);
+            //    Console.ReadKey();
+            //}
+            //Console.WriteLine("Another foreach");
+            //Console.ReadKey();
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine("foreach " + item);
+            //    Console.ReadKey();
+            //}
+
+            //Console.WriteLine(result.FirstOrDefault());
+
+            //Console.WriteLine("ToList");
+            //Console.ReadKey();
+            //var lResult = result.ToList();
+            //Console.WriteLine("continue");
+            //Console.ReadKey();
+            //Console.WriteLine($@"
+            //Count: {lResult.Count}
+            //0: {lResult[0]}
+            //");
+            //Console.WriteLine("Finish");
+            //Console.ReadKey();
+
+            // ================== grouping ====================
+            List<string> names = new()
+            {
+                "Zvi",
+                "Lia",
+                "Reuven",
+                "Eliahu z\"l",
+                "Sarit",
+                "David", // Dani's kids
+                "Avraham",
+                "Mira",
+                "Moshe", // Dani's nephews
+                "Shim'on",
+                "Levy",
+                "Yehuda",
+                "Issachar",
+                "Zvulun",
+                "Dan", // Yaakov kids
+                "Naftali",
+                "Gad",
+                "Asher",
+                "Dina",
+                "Yossef",
+                "Byniamin" // More Yaakov kids
+            };
+
+            //var temp = from name in names
+            //           group name.Substring(1) by name[0];
+            //var dict = from grp in temp
+            //           orderby grp.Key
+            //           select grp;
+            // ******* can be concatenated into a single LINQ request by "into" syntax
+            var dict = from name in names
+                       group name.Substring(1) by name[0]
+                       into grp // here it is
+                       orderby grp.Count() // grp.Key
+                       select grp;
+
+
+            foreach (var item in dict)
+            {
+                var key = item.Key;
+                Console.WriteLine(key + ":");
+                Console.ReadKey();
+                int count = 0;
+                foreach (var elem in item)
+                    Console.WriteLine("  " + ++count + ". " + key + elem);
+                Console.ReadKey();
+            }
+
+            foreach (var item in from name in names
+                                 group name.Substring(1) by name[0]
+                                 into grp
+                                 orderby grp.Key
+                                 select grp) ;
+
+            Console.WriteLine(dict.Average(grp => grp.Count()));
+
+            var result3 = names.GroupBy(name => name[0], name => name.Substring(1))
+                               .OrderBy(grp => grp.Key);
+        }
+
+        static IEnumerable<IGrouping<char, string>> foo(IEnumerable<string> coll)
+            => from name in coll
+               group name.Substring(1) by name[0]
+               into grp
+               orderby grp.Key
+               select grp;
+    }
+}
--- WPF1/MainWindow.xaml.cs
+++ WPF1/MainWindow.xaml.cs
@@ -23,6 +23,47 @@ namespace WPF1
         public MainWindow()
         {
             InitializeComponent();
+
+            BtnCancel.Click += BtnCancel_Click;
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ChkBoxApprove.IsChecked = !ChkBoxApprove.IsChecked;
+            ((Button)sender).Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I am here", "OK BUTTON", MessageBoxButton.OK);
+        }
+
+        private void BtnOK_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ChkBoxApprove_Checked(object sender, RoutedEventArgs e)
+        {
+            BtnOK.IsEnabled = ((CheckBox)sender).IsChecked ?? false;
+        }
+
+        private void ChkBoxApprove_Unchecked(object sender, RoutedEventArgs e)
+        {
+            BtnOK.IsEnabled = ((CheckBox)sender).IsChecked ?? false;
+            e.Handled = true;
+        }
+
+        static Random random = new();
+        private void BtnCancel_MouseMove(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            Size size = (btn.Parent as Grid).RenderSize;
+            Thickness margin = btn.Margin;
+            margin.Left = random.NextDouble() * (size.Width - btn.ActualWidth);
+            margin.Top = random.NextDouble() * (size.Height - btn.ActualHeight);
+            btn.Margin = margin;
+            
         }
     }
 }
